5b9206fcbf04dd774ef2dd4bc556acdb8c372d04
Evgniy Kochetkov <ekochetkov.work@gmail.com>
2022-06-19 13:40:41 +0300

    Fix broken dependency on Ubuntu build

--- project.clj
+++ project.clj
@@ -2,6 +2,7 @@
   :description "Clock application on fullstack Clojure/ClojureScript"
   :url "https://clojure-clock-fullstack-app.herokuapp.com/"
   :dependencies [[org.clojure/clojure "1.11.1"]
+                 [javax.xml.bind/jaxb-api "2.3.1"]
                  [http-kit "2.1.18"]
                  [compojure "1.3.4"]
                  [hiccup "1.0.5"]]
